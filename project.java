254db2b5cf9c0efc7500ff818a0e5272f69b68bf
Mohammed Elkazzaz <mohammed.elkazzaz14@gmail.com>
2023-09-03 14:31:57 +0300

    Hello

--- project.java
+++ project.java
@@ -2,5 +2,6 @@ public class project {
     public static void main(String[] args) {
         System.out.println("Test For GitHub :)");
         System.out.println("Under Construction !!");
+        System.out.println("hello");
     }
 }
