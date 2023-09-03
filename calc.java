7cee1dfa9406ddeb513b74bd6bad0969f1f23e79
Mohammed Elkazzaz <mohammed.elkazzaz14@gmail.com>
2023-09-03 04:35:04 +0300

    Writing the first lines in Calc

---
+++ calc.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class calc {
+    public static void main(String[] args) {
+        Scanner choice = new Scanner(System.in);
+        System.out.println("Choose any operation:-");
+        System.out.println("[1] Addition");
+        System.out.println("[2] Subtraction");
+        System.out.println("[3] Multiplication");
+        System.out.println("[4] Division");
+
+        int varChoice = choice.nextInt();
+        
+
+    }
+}
