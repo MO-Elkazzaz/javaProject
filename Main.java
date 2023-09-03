d75e49ac8fd30fd5b7499b0606ce58fb8422e7a7
Mohammed Elkazzaz <mohammed.elkazzaz14@gmail.com>
2023-09-03 04:21:06 +0300

    Editing Classes names

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class test {
+public class Main {
     public static void main(String[] args) {
 
         Scanner again = new Scanner(System.in);
--- project.java
+++ project.java
@@ -1,4 +1,4 @@
-public class Main {
+public class project {
     public static void main(String[] args) {
         System.out.println("Test For GitHub :)");
         System.out.println("Under Construction !!");
