c0b21f3952bc3c4dca6a1f7b155f03e0964aa3f6
Mohammed Elkazzaz <mohammed.elkazzaz14@gmail.com>
2023-09-04 18:44:25 +0300

    The base code for the calculator

--- calc.java
+++ calc.java
@@ -1,15 +1,49 @@
 import java.util.Scanner;
 public class calc {
     public static void main(String[] args) {
-        Scanner choice = new Scanner(System.in);
-        System.out.println("Choose any operation:-");
-        System.out.println("[1] Addition");
-        System.out.println("[2] Subtraction");
-        System.out.println("[3] Multiplication");
-        System.out.println("[4] Division");
+        calc calculator = new calc();
 
-        int varChoice = choice.nextInt();
-        
+        int addition = calculator.add(5, 2);
+        int subtraction = calculator.subtract(5, 2);
+        int multiplication = calculator.multiply(5, 6);
+        int division = calculator.division(4, 5);
 
+        System.out.println(addition);
+        System.out.println(subtraction);
+        System.out.println(multiplication);
+        System.out.println(division);
+    }
+
+
+    public int add(int x , int y) {
+        return x + y;
+    }
+
+
+
+    public int subtract(int x , int y){
+        if (x >= y) {
+            return x - y;
+        }
+        else {
+            return y - x;
+        }
+    }
+
+
+
+    public int multiply(int x , int y) {
+        return x * y;
+    }
+
+
+
+    public int division(int x , int y){
+        if (x >= y) {
+            return x / y;
+        }
+        else {
+            return y / x;
+        }
     }
 }
